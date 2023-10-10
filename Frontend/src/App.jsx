d8f8d139fb6abab80db2f7367071809ec378470f
Srj912 <Sreeraj912@yahoo.com>
2023-10-10 19:39:34 +0530

    Updated React + Node

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const App = () => {
+    const [data, setData] = useState([]);
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        phoneno: "",
+    });
+    const [status, setStatus] = useState(true);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/users")
+            .then((response) => response.json())
+            .then((json) => {
+                setData(json);
+            });
+    }, [status]);
+
+    const changeHandler = (e) => {
+        setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const submitHandler = () => {
+        axios
+            .post("http://localhost:3000/users", user)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        setStatus(!status);
+        setUser({
+            name: "",
+            email: "",
+            phoneno: "",
+        });
+    };
+    const deleteHandler = (id) => {
+        console.log(id);
+        axios
+            .delete(`http://localhost:3000/users/${id}`)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        setStatus(!status);
+    };
+
+    return (
+        <>
+            <h2>create table</h2>
+            <input
+                placeholder="name"
+                onChange={changeHandler}
+                name="name"
+                value={user.name}
+            ></input>
+            <input
+                placeholder="email"
+                onChange={changeHandler}
+                name="email"
+                value={user.email}
+            ></input>
+            <input
+                placeholder="phoneno"
+                onChange={changeHandler}
+                name="phoneno"
+                value={user.phoneno}
+            ></input>
+            <button onClick={submitHandler}>Submit</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>phoneno</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.length > 0 ? (
+                        data.map(({ id, name, email, phoneno }, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{name}</td>
+                                    <td>{email}</td>
+                                    <td>{phoneno}</td>
+                                    <td>
+                                        <button
+                                            onClick={() => deleteHandler(id)}
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    ) : (
+                        <tr>
+                            <td>no data</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </>
+    );
+};
+export default App;
